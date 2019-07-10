2665d223c4c5e7dfa435ab36672a5e9dd5085aa4
esurat20 <esurat20@gmail.com>
2019-07-10 13:25:27 +0300

    implementing export to xls

---
+++ lib/etalon_equipment_web/views/exports/equipment_view.ex
@@ -0,0 +1,39 @@
+defmodule EtalonEquipmentWeb.Exports.EquipmentView do
+  use EtalonEquipmentWeb, :view
+
+  alias Elixlsx.{Workbook, Sheet}
+
+  @header [
+    "Наименование",
+    "Количество",
+    "Единица измерения",
+    "Стоимость",
+    "Работник",
+    "Категория",
+    "Поставщик"
+  ]
+
+  def render("report.xlsx", %{equipments: equipments}) do
+    report_generator(equipments)
+    |> Elixlsx.write_to_memory("report.xlsx")
+    |> elem(1)
+    |> elem(1)
+  end
+
+  def report_generator(equipments) do
+    rows = equipments |> Enum.map(&row(&1))
+    %Workbook{sheets: [%Sheet{name: "Equipments", rows: [@header] ++ rows}]}
+  end
+
+  def row(equipment) do
+    [
+      equipment.name,
+      to_string(equipment.amount),
+      equipment.count_type,
+      equipment.cost,
+      equipment.vendor,
+      equipment.employee.name,
+      equipment.category.name
+    ]
+  end
+end
--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule EtalonEquipment.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:plug_cowboy, "~> 1.0"}
+      {:plug_cowboy, "~> 1.0"},
+      {:elixlsx, "~> 0.4.2"}
     ]
   end
 
